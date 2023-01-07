1633731882a0b4eccff27e418f300b3859f84e57
leo <leo493852107@163.com>
2023-01-07 23:12:03 +0800

    first commit

---
+++ leo-blog/src/main/java/com.leo23/LeoBlogApplication.java
@@ -0,0 +1,11 @@
+package com.leo23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeoBlogApplication.class, args);
+    }
+}
