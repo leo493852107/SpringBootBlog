f04d36872939ea4c44ded89400e11898b5be614e
leo <leo493852107@163.com>
2023-01-08 19:54:18 +0800

    FastJson配置

--- leo-framework/src/main/java/com/leo23/config/WebConfig.java
+++ leo-framework/src/main/java/com/leo23/config/WebConfig.java
@@ -1,8 +1,17 @@
 package com.leo23.config;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -20,5 +29,25 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 // 跨域允许的时间
                 .maxAge(3600);
+    }
+
+    @Bean
+    public HttpMessageConverter fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        SerializeConfig.getGlobalInstance().put(Long.class, ToStringSerializer.instance);
+
+        fastJsonConfig.setSerializeConfig(SerializeConfig.getGlobalInstance());
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(fastJsonHttpMessageConverters());
     }
 }
