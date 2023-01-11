8826fbf24dd7361373874ffbdd654976d3bbb828
leo <leo493852107@163.com>
2023-01-11 21:47:18 +0800

    接口参数配置

--- leo-blog/src/main/java/com.leo23/controller/CommentController.java
+++ leo-blog/src/main/java/com.leo23/controller/CommentController.java
@@ -4,12 +4,17 @@ import com.leo23.constants.SystemConstants;
 import com.leo23.domain.ResponseResult;
 import com.leo23.domain.entity.Comment;
 import com.leo23.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/comment")
+@Api(tags = "评论")
 public class CommentController {
     @Resource
     private CommentService commentService;
@@ -25,6 +30,10 @@ public class CommentController {
     }
 
     @GetMapping("/linkCommentList")
+    @ApiOperation(value = "友链评论列表", notes = "获取1页友链评论")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", value = "页码"),
+            @ApiImplicitParam(name = "pageSize", value = "每页记录数")})
     public ResponseResult linkCommentList(Integer pageNum, Integer pageSize) {
         return commentService.commentList(SystemConstants.LINK_COMMENT, null, pageNum, pageSize);
     }
