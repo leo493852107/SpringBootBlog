206ae39993c60d881bcdbe0afdddeb78d8352de6
leo <leo493852107@163.com>
2023-01-11 20:53:48 +0800

    同步redis到数据库中去

--- leo-blog/src/main/java/com.leo23/job/TestJob.java
+++ leo-blog/src/main/java/com.leo23/job/TestJob.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TestJob {
-    @Scheduled(cron = "0/5 * * * * ?")
-    public void testJob() {
-        System.out.println("555");
-    }
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    public void testJob() {
+//        System.out.println("555");
+//    }
 }
---
+++ leo-blog/src/main/java/com.leo23/job/UpdateViewCount.java
@@ -0,0 +1,34 @@
+package com.leo23.job;
+
+import com.leo23.constants.ArticleConstants;
+import com.leo23.domain.entity.Article;
+import com.leo23.service.ArticleService;
+import com.leo23.utils.RedisCache;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class UpdateViewCount {
+    @Resource
+    private RedisCache redisCache;
+    @Resource
+    private ArticleService articleService;
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void updateArticleViewCount() {
+        // 获取redis的浏览量
+        Map<String, Integer> viewCountMap = redisCache.getCacheMap(ArticleConstants.ARTICLE_VIEW_COUNT);
+        List<Article> articleList = viewCountMap.entrySet()
+                .stream()
+                .map(entry -> new Article(Long.valueOf(entry.getKey()), entry.getValue().longValue()))
+                .collect(Collectors.toList());
+        // 更新到数据库
+        articleService.updateBatchById(articleList);
+    }
+
+}
